a8466486aa453467ef8e9a94528c793dbe58f65c
Gustavo Oliveira <gustavoooliveiradecarvalho@gmail.com>
2024-11-08 13:16:15 -0400

    feat: add connection to a MySQL and add DotEnv
    - rm gocommerce bin because on all run
    - the bin changes, just run `make run`

--- cmd/main.go
+++ cmd/main.go
@@ -1,14 +1,42 @@
 package main
 
 import (
+	"database/sql"
 	"log"
 
 	"github.com/Gustavicho/gocommerce/cmd/api"
+	"github.com/Gustavicho/gocommerce/config"
+	"github.com/Gustavicho/gocommerce/db"
+	"github.com/go-sql-driver/mysql"
 )
 
 func main() {
-	server := api.NewAPIService(":8080", nil)
+	db, err := db.NewMySQLStorage(mysql.Config{
+		User: config.Envs.DBUser,
+		Passwd: config.Envs.DBPassword,
+		Addr: config.Envs.DBAddress,
+		DBName: config.Envs.DBName,
+		Net: "tcp",
+		AllowNativePasswords: true,
+		ParseTime: true,
+	})
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	initStorage(db)
+
+	server := api.NewAPIService(":8080", db)
 	if err := server.Run(); err != nil {
 		log.Fatal(err)
 	}
+}
+
+
+func initStorage(db *sql.DB) {
+	if err := db.Ping(); err != nil {
+		log.Fatal(err)
+	}
+
+	log.Println("Successfully connected to database")
 }
---
+++ config/env.go
@@ -0,0 +1,42 @@
+package config
+
+import (
+	"fmt"
+	"os"
+
+	"github.com/joho/godotenv"
+)
+
+type Config struct {
+	PublicHost string
+	Port string
+	
+	DBUser string
+	DBPassword string
+	DBAddress string
+	DBName string
+}
+
+var Envs Config = initConfig()
+
+func initConfig() Config {
+	godotenv.Load()
+	
+	return Config{
+		PublicHost: getEnv("PUBLIC_HOST", "localhost"),
+		Port: getEnv("PORT", "8080"),
+
+		DBUser: getEnv("DB_USER", "mysql"),
+		DBPassword: getEnv("DB_PASSWORD", ""),
+		DBAddress: fmt.Sprintf("%s:%s", getEnv("DB_HOST", "localhost"), getEnv("DB_PORT", "3306")),
+		DBName: getEnv("DB_NAME", "postgres"),
+	}
+}
+
+func getEnv(key, fallback string) string {
+	if value, ok := os.LookupEnv(key); ok {
+		return value
+	}
+
+	return fallback
+}
