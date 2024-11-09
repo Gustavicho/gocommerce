60753fe0a7f678c37fcf4c3ddbc2612f97d3f3a2
Gustavo Oliveira <gustavoooliveiradecarvalho@gmail.com>
2024-11-09 09:51:32 -0400

    fix: query By email and ID
    - now they return the user if it exist
    - rm createdAt field on CreateUser
    - change how scanRows works

--- service/user/store.go
+++ service/user/store.go
@@ -3,7 +3,6 @@ package user
 import (
 	"database/sql"
 	"fmt"
-	"time"
 
 	"github.com/Gustavicho/gocommerce/types"
 )
@@ -18,15 +17,8 @@ func NewStore(db *sql.DB) *Store {
 	}
 }
 
-func (s *Store) CreateUser(user *types.User) error {
-	_, err := s.db.Exec(
-		"INSERT INTO users (first_name, last_name, email, password, created_at) VALUES (?, ?, ?, ?, ?)",
-		user.FirstName,
-		user.LastName,
-		user.Email,
-		user.Password,
-		time.Now(),
-	)
+func (s *Store) CreateUser(user types.User) error {
+	_, err := s.db.Exec("INSERT INTO users (first_name, last_name, email, password) VALUES (?, ?, ?, ?)", user.FirstName, user.LastName, user.Email, user.Password)
 	if err != nil {
 		return err
 	}
@@ -39,10 +31,11 @@ func (s *Store) GetUserByEmail(email string) (*types.User, error) {
 	if err != nil {
 		return nil, err
 	}
+	defer rows.Close()
 
 	u := new(types.User)
 	for rows.Next() {
-		u, err = scanRowIntoUser(rows)
+		err = scanRowsIntoUser(rows, u)
 		if err != nil {
 			return nil, err
 		}
@@ -55,15 +48,16 @@ func (s *Store) GetUserByEmail(email string) (*types.User, error) {
 	return u, nil
 }
 
-func (s *Store) GetUserByID(id uint) (*types.User, error) {
+func (s *Store) GetUserByID(id int) (*types.User, error) {
 	rows, err := s.db.Query("SELECT * FROM users WHERE id = ?", id)
 	if err != nil {
 		return nil, err
 	}
+	defer rows.Close()
 
 	u := new(types.User)
 	for rows.Next() {
-		u, err = scanRowIntoUser(rows)
+		err = scanRowsIntoUser(rows, u)
 		if err != nil {
 			return nil, err
 		}
@@ -75,20 +69,20 @@ func (s *Store) GetUserByID(id uint) (*types.User, error) {
 
 	return u, nil
 }
-func scanRowIntoUser(row *sql.Rows) (*types.User, error) {
-	user := new(types.User)
 
-	err := row.Scan(
+func scanRowsIntoUser(rows *sql.Rows, user *types.User) error {
+	err := rows.Scan(
 		&user.ID,
 		&user.FirstName,
 		&user.LastName,
 		&user.Email,
 		&user.Password,
 		&user.CreatedAt,
+		&user.UpdatedAt,
 	)
 	if err != nil {
-		return nil, err
+		return err
 	}
 
-	return user, nil
+	return nil
 }
